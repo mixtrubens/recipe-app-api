5efd4956393f81b2e9040b0fbc03ceb8e235512e
Rubens <megapixmode@gmail.com>
2022-05-12 17:26:00 -0300

    Add validation for email field OK

--- app/core/models.py
+++ app/core/models.py
@@ -7,12 +7,13 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
+        if not email:
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
