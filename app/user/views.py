bd8c218e64626b9214b3e63a32ec4725a1140b99
Rubens <megapixmode@gmail.com>
2022-05-20 17:38:30 -0300

    207 OK

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
+<<<<<<< HEAD
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
+=======
+from user.serializer import UserSerializer
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializer import UserSerializer, AuthTokenSerializer
+>>>>>>> f1b4e1e4003d46d6f2f776e7c9194cf8a04a2209
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -10,6 +18,14 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+<<<<<<< HEAD
+=======
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+>>>>>>> f1b4e1e4003d46d6f2f776e7c9194cf8a04a2209
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user"""
     serializer_class = AuthTokenSerializer
