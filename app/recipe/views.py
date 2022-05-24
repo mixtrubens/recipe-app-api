07cca09014b76b43a79803bac86bde3b43f895de
Rubens <megapixmode@gmail.com>
2022-05-24 16:56:30 -0300

    218. Implement feature for creating ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,3 +35,14 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage ingredients in the database"""
+    ...
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
