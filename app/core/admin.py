30258abca2fd39847e29d0bdeabfc21c518cdcde
Rubens <megapixmode@gmail.com>
2022-05-27 09:59:23 -0300

    233. Implement feature to filter recipes 2

--- app/core/admin.py
+++ app/core/admin.py
@@ -22,8 +22,15 @@ class UserAdmin(BaseUserAdmin):
     ),
     (_('Important dates'), {'fields': ('last_login',)}),
 )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
